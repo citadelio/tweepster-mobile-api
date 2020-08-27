ceef92ed7def448d859d2e0507116f5cee81c90f
citadelio <warrakehussein@gmail.com>
2020-08-27 21:20:34 +0100

    App update

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const isRequestFromMobile = require("../middleware/mobilecheck");
 const UserModel = require('../models/User');
 const { twitterConfig } = require('../middleware/helperFunctions');
 
-router.get('/detail', protectedRoute, async(req, res)=>{
+router.get('/detail', protectedRoute, isRequestFromMobile,  async(req, res)=>{
     try{
         const user = await UserModel.findOne({_id:req.userid})
         if(!user){
@@ -25,6 +25,37 @@ router.get('/detail', protectedRoute, async(req, res)=>{
         // get user details from twitter
        const twitterUser =  await client.get("account/verify_credentials");
         return res.json({user: twitterUser, status:true})
+    }
+    catch(err){
+      return res.json({
+        errors: [
+          {
+            msg: "An error occurred, try again",
+            err
+          }
+        ]
+      });
+    }
+});
+
+router.get('/fetch-friends', protectedRoute, isRequestFromMobile, async(req, res)=>{
+    try{
+        const user = await UserModel.findOne({_id:req.userid})
+        if(!user){
+            return res.json({
+                errors: [
+                  {
+                    msg: "User not found",
+                  }
+                ]
+              });
+        }
+       
+        
+        const client = twitterConfig(user.authtoken, user.authsecret)
+        // get user details from twitter
+       const friends =  await client.get("friends/list");
+        return res.json(friends)
     }
     catch(err){
       return res.json({
