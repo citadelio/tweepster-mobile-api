a9877c4eb47b7cd1455a178b80f2752c6ea0c59f
citadelio <warrakehussein@gmail.com>
2020-08-30 14:43:57 +0100

    App Update

--- routes/users.js
+++ routes/users.js
@@ -78,6 +78,45 @@ router.get('/fetch-friends/:count',protectedRoute, isRequestFromMobile,  async(r
     }
 });
 
+router.get('/fetch-follower/:count',protectedRoute, isRequestFromMobile,  async(req, res)=>{
+    try{
+      let cursor = req.params.count;
+      const user = await UserModel.findOne({_id:req.userid})
+      if(!user){
+        return res.json({
+          errors: [
+            {
+              msg: "User not found",
+            }
+          ]
+        });
+      }
+      
+      
+      const client = twitterConfig(user.authtoken, user.authsecret)
+      // const client = twitterConfig("586786732-K9o4MwJp8IyWA8GEqcOSBd75QTmRFrO1HPYs7pB4", "a5nLNe58c0bixr87EMI7x99AOIDGK67GpJs3LhnPX512c")
+      // get user details from twitter
+       const followersIds =  await client.get("followers/ids",{cursor, count:20});
+        
+       const followersList =  await client.get("friendships/lookup",{user_id:followersIds.ids.join()});
+       return res.json({
+         followers:followersList,
+         previous : followersIds.previous_cursor_str,
+         next : followersIds.next_cursor_str
+       })
+    }
+    catch(err){
+      return res.json({
+        errors: [
+          {
+            msg: "An error occurred, try again",
+            err
+          }
+        ]
+      });
+    }
+});
+
 router.get('/user-details/:id',protectedRoute, isRequestFromMobile,  async(req, res)=>{
       try{
         let user_id = req.params.id;
