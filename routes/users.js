2a6d0a380941d4c5413452b2546d48477b191f90
citadelio <warrakehussein@gmail.com>
2020-08-30 12:20:52 +0100

    App Update

--- routes/users.js
+++ routes/users.js
@@ -39,6 +39,7 @@ router.get('/detail', protectedRoute, isRequestFromMobile,  async(req, res)=>{
 });
 
 router.get('/fetch-friends/:count',protectedRoute, isRequestFromMobile,  async(req, res)=>{
+// router.get('/fetch-friends/:count',  async(req, res)=>{
     try{
       let cursor = req.params.count;
       const user = await UserModel.findOne({_id:req.userid})
@@ -56,8 +57,14 @@ router.get('/fetch-friends/:count',protectedRoute, isRequestFromMobile,  async(r
       const client = twitterConfig(user.authtoken, user.authsecret)
       // const client = twitterConfig("586786732-K9o4MwJp8IyWA8GEqcOSBd75QTmRFrO1HPYs7pB4", "a5nLNe58c0bixr87EMI7x99AOIDGK67GpJs3LhnPX512c")
       // get user details from twitter
-       const friends =  await client.get("friends/list",{cursor});
-        return res.json(friends)
+       const friendIds =  await client.get("friends/ids",{cursor, count:20});
+        
+       const friendList =  await client.get("friendships/lookup",{user_id:friendIds.ids.join()});
+       return res.json({
+         friends:friendList,
+         previous : friendIds.previous_cursor_str,
+         next : friendIds.next_cursor_str
+       })
     }
     catch(err){
       return res.json({
@@ -70,5 +77,6 @@ router.get('/fetch-friends/:count',protectedRoute, isRequestFromMobile,  async(r
       });
     }
 });
+
 
 module.exports = router;
